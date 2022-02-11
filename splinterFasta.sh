9c452eeb7d290022fb23bbb49374afd681525522
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2022-02-11 22:48:13 +0000

    Update splinterFasta.sh
    added flags

--- splinterFasta.sh
+++ splinterFasta.sh
@@ -1,4 +1,14 @@
-cat $INPUT | awk '{
+#!/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -f|--fasta) fasta="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+cat $fasta | awk '{
         if (substr($0, 1, 1)==">") {filename=(substr($0,2) ".fa")}
         print $0 > filename
 }'

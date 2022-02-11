c1323a6f8e374abe6040d0e9b79fbaf0c9286bc9
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2022-02-11 22:40:25 +0000

    Update extractSeq.py

--- extractSeq.py
+++ extractSeq.py
@@ -1,12 +1,18 @@
 from Bio import SeqIO
 
-# argparse here
+import argparse
 
-fasta = 
-multifasta = 
+parser = argparse.ArgumentParser(description="Count the lengths of sequences in a fasta.")
+parser.add_argument('-t', '--target', help='Target sequence for extraction.')
+parser.add_argument('-f'. '--fasta', help='FASTA to extract sequence from.')
+args = parse.parse_args()
+
+target = args.target
+multifasta = args.multifasta
 
 with open(fasta + ".fasta","w") as f:
         for seq_record in SeqIO.parse(multifasta, "fasta"):
 		if seq_record.id == fasta:
 	                f.write(str(seq_record.id) + "\n")
 			f.write(str(seq_record.seq) + "\n")
+f.close()

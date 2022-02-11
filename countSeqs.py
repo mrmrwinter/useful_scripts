d9b7cb84df0dd9fcc8f701a0a9b3bc1d7f067519
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2022-02-11 22:37:21 +0000

    Update countSeqs.py
    added argparse

--- countSeqs.py
+++ countSeqs.py
@@ -1,5 +1,8 @@
 import argparse
 
+parser = argparse.ArgumentParser(description="Count the lengths of sequences in a fasta.")
+parser.add_argument('-f', '--fasta', help="FASTA input")
+args = parse.parse_args()
 
 fasta = args.fasta
 

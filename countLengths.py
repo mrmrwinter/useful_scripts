a4b4f9f4bf5d42655ace439766caa49b5c30e82b
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2022-02-11 22:36:14 +0000

    Update countLengths.py
    Added argparse

--- countLengths.py
+++ countLengths.py
@@ -1,4 +1,9 @@
 import Bio.SeqIO as IO
+import argparse
+
+parser = argparse.ArgumentParser(description="Count the lengths of sequences in a fasta.")
+parser.add_argument('-f', '--fasta', help="FASTA input")
+args = parse.parse_args()
 
 fasta = args.fasta()
 
